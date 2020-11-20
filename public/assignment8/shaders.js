6a5d33f850b48ed93d6d901202c67ef9c37a4972
Dennis Kats <denniskats43@gmail.com>
2020-11-20 03:51:14 -0500

    Finished A8

---
+++ public/assignment8/shaders.js
@@ -0,0 +1,51 @@
+/**
+ * @param {WebGLRenderingContext} gl
+ */
+const initializeShaderProgram = (gl) => {
+    const vertexShaderCode = document.getElementById("vertex-shader").textContent;
+    const fragmentShaderCode = document.getElementById("fragment-shader").textContent;
+
+    const vertexShader = loadShader(gl, gl.VERTEX_SHADER, vertexShaderCode);
+    const fragmentShader = loadShader(gl, gl.FRAGMENT_SHADER, fragmentShaderCode);
+
+    const shaderProgram = gl.createProgram();
+    gl.attachShader(shaderProgram, vertexShader);
+    gl.attachShader(shaderProgram, fragmentShader);
+    gl.linkProgram(shaderProgram);
+
+    return shaderProgram;
+};
+
+/**
+ *
+ * @param {WebGLRenderingContext} gl
+ * @param {number} type
+ * @param {string} source
+ */
+const loadShader = (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+
+    return shader;
+};
+
+/**
+ *
+ * @param {WebGLRenderingContext} gl
+ * @param {WebGLProgram} shaderProgram
+ */
+const getProgramParameters = (gl, shaderProgram) => {
+    return {
+        program: shaderProgram,
+        attribLocations: {
+            vertexPosition: gl.getAttribLocation(shaderProgram, "aVertexPosition"),
+            // vertexColor: gl.getAttribLocation(shaderProgram, "aVertexColor"),
+            textureCoord: gl.getAttribLocation(shaderProgram, "aTextureCoord"),
+        },
+        uniformLocations: {
+            projectionMatrix: gl.getUniformLocation(shaderProgram, "uProjectionMatrix"),
+            modelViewMatrix: gl.getUniformLocation(shaderProgram, "uModelViewMatrix"),
+        },
+    };
+};
